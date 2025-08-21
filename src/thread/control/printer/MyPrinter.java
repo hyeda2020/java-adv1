9c064647165065d2fc35cb26e2bec99ed41a18d3
hyeda2020 <hyeda2020@gmail.com>
2025-08-21 22:26:52 +0900

    2. 스레드 제어와 생성주기 - 프린터 예제

---
+++ src/thread/control/printer/MyPrinter.java
@@ -0,0 +1,52 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class MyPrinter {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = scanner.nextLine();
+            if (input.equals("q")) {
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+
+            while (!Thread.interrupted()) {
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                try {
+                    String job = jobQueue.poll();
+                    System.out.println("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    System.out.println("출력 완료: " + job);
+                } catch (InterruptedException e) {
+                    System.out.println("인터럽트 발생");
+                }
+            }
+            System.out.println("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
