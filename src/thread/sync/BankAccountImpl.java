2e4f3a73448036eb780a30ec6cd874642eb1a5d3
hyeda2020 <hyeda2020@gmail.com>
2025-11-15 21:27:05 +0900

    동기화 - synchronized 코드블럭 적용

--- src/thread/sync/BankAccountImpl.java
+++ src/thread/sync/BankAccountImpl.java
@@ -9,23 +9,32 @@ public class BankAccountImpl implements BankAccount{
     }
 
     @Override
-    public synchronized boolean withdraw(int amount) {
+//    public synchronized boolean withdraw(int amount) {
+    public boolean withdraw(int amount) {
         System.out.println("거래 시작: " + getClass().getSimpleName());
-        System.out.println("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
-        if (balance < amount) {
-            System.out.println("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
-            return false;
-        }
-        System.out.println("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
 
-        try {
-            Thread.sleep(1000); // 출금에 걸리는 시간으로 가정
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        /**
+         * 메서드 전체에 synchronized 를 적용하는 것 보다는
+         * 꼭 필요한 구간에만 적용하는게 성능적으로 더 이득.
+         */
+        synchronized (this) {
+            System.out.println("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                System.out.println("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            System.out.println("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+
+            try {
+                Thread.sleep(1000); // 출금에 걸리는 시간으로 가정
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         balance = balance - amount;
         System.out.println("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
+
         System.out.println("거래 종료");
         return true;
     }
